162b103caf608525c8a1b946955a5f7013e13c21
HassineHelmi <helmi.hassine1@gmail.com>
2023-12-25 12:42:44 +0100

    successfull connection established with firebase, all the products informations are correctly displayed to the user

--- app/src/main/java/com/example/tunisiepromo/ProductAdapter.java
+++ app/src/main/java/com/example/tunisiepromo/ProductAdapter.java
@@ -12,34 +12,51 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
-public class ProductAdapter  extends  RecyclerView.Adapter<ProductAdapter.ProductViewHolder>{
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
     private List<Shoe> shoesList;
     private Context context;
+
     public ProductAdapter(List<Shoe> shoesList, Context context) {
         this.shoesList = shoesList;
         this.context = context;
     }
+
     @NonNull
     @Override
     public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_item, parent, false);
         return new ProductViewHolder(view);
     }
+
     @Override
     public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
         Shoe shoe = shoesList.get(position);
+
         // Bind data to views
         holder.productName.setText(shoe.getName());
-        holder.productPrice.setText(String.valueOf(shoe.getPrice()));
-        holder.productCategory.setText(String.valueOf(shoe.getCategory()));
+
+        // Format and display the price with currency symbol
+        String formattedPrice = formatPrice(shoe.getPriceAP());
+        holder.productPrice.setText(formattedPrice);
+
+        holder.productCategory.setText(shoe.getCategory());
         holder.promotion.setText(String.valueOf(shoe.getPromotionPercentage()));
+
         // Use Glide to load the image from the URL
         Glide.with(context)
-                .load(shoe.getImageUrl())
+                .load(shoe.getImage_url())
                 .into(holder.productImage);
     }
+
+    private String formatPrice(double price) {
+        // Example: Format the price with currency symbol and two decimal places
+        DecimalFormat decimalFormat = new DecimalFormat("$#.00");
+        return decimalFormat.format(price);
+    }
+
     @Override
     public int getItemCount() {
         return shoesList.size();
@@ -52,15 +69,13 @@ public class ProductAdapter  extends  RecyclerView.Adapter<ProductAdapter.Produc
         TextView productCategory;
         TextView promotion;
 
-
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
             productImage = itemView.findViewById(R.id.imageViewProduct);
             productName = itemView.findViewById(R.id.textViewProductName);
             productPrice = itemView.findViewById(R.id.textViewProductPrice);
-            productCategory= itemView.findViewById(R.id.textViewProductCategory);
-            promotion= itemView.findViewById(R.id.textViewPromotionPercentage);
+            productCategory = itemView.findViewById(R.id.textViewProductCategory);
+            promotion = itemView.findViewById(R.id.textViewPromotionPercentage);
         }
     }
-
 }
--- app/src/main/java/com/example/tunisiepromo/Shoe.java
+++ app/src/main/java/com/example/tunisiepromo/Shoe.java
@@ -1,50 +1,47 @@
 package com.example.tunisiepromo;
 
-public class Shoe {
-    private String name;
-    private String imageUrl;
-    private String category;
-    private String description;
-    private String price;
-    private String productId;
+import java.util.List;
 
-    private double promotionPercentage; // Corrected field name
+public class Shoe {
+    private String shoeId;
+    private String Name;
+    private String description;
+    private String image_url;  // Ensure that the field names match the keys in the JSON data
+    private double priceAP;
+    private double priceBP;
+    private int promotionPercentage;
+    private List<Integer> size;
+    private String category;
 
     public Shoe() {
     }
 
-    public Shoe(String name, String imageUrl, String category, String description, String price, String productId, double promotionPercentage) {
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.category = category;
+    public Shoe(String shoeId, String name, String description, String imageUrl, double priceAP, double priceBP, int promotionPercentage, List<Integer> size, String category) {
+        this.shoeId = shoeId;
+        this.Name = name;
         this.description = description;
-        this.price = price;
-        this.productId = productId;
-        this.promotionPercentage = promotionPercentage; // Added the promotionPercentage field to the constructor
+        this.image_url = imageUrl;
+        this.priceAP = priceAP;
+        this.priceBP = priceBP;
+        this.promotionPercentage = promotionPercentage;
+        this.size = size;
+        this.category = category;
+    }
+
+    public String getShoeId() {
+        return shoeId;
+    }
+
+    public void setShoeId(String shoeId) {
+        this.shoeId = shoeId;
     }
 
     public String getName() {
-        return name;
+        return Name;
     }
 
     public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
+        this.Name = name;
     }
 
     public String getDescription() {
@@ -55,27 +52,51 @@ public class Shoe {
         this.description = description;
     }
 
-    public String getPrice() {
-        return price;
+    public String getImage_url() {
+        return image_url;
     }
 
-    public void setPrice(String price) {
-        this.price = price;
+    public void setImageUrl(String imageUrl) {
+        this.image_url = imageUrl;
     }
 
-    public String getProductId() {
-        return productId;
+    public double getPriceAP() {
+        return priceAP;
     }
 
-    public void setProductId(String productId) {
-        this.productId = productId;
+    public void setPriceAP(double priceAP) {
+        this.priceAP = priceAP;
     }
 
-    public double getPromotionPercentage() {
+    public double getPriceBP() {
+        return priceBP;
+    }
+
+    public void setPriceBP(double priceBP) {
+        this.priceBP = priceBP;
+    }
+
+    public int getPromotionPercentage() {
         return promotionPercentage;
     }
 
-    public void setPromotionPercentage(double promotionPercentage) {
+    public void setPromotionPercentage(int promotionPercentage) {
         this.promotionPercentage = promotionPercentage;
+    }
+
+    public List<Integer> getSize() {
+        return size;
+    }
+
+    public void setSize(List<Integer> size) {
+        this.size = size;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
